c4faedf541e3b6f05e45a7a75bafaf7f4a1e2c10
Audrey Tang <audreyt@audreyt.org>
2019-04-12 11:45:06 +0800

    * Launch time

--- js/countdown.js
+++ js/countdown.js
@@ -30,7 +30,7 @@ CountdownTimer.prototype={
 function CDT(){
 
  // Set countdown limit
- var tl = new Date('2019/04/16 00:00:00');
+ var tl = new Date('2019/04/15 10:00:00');
 
  // You can add time's up message here
  var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div><span class="number end"></span></span>');
