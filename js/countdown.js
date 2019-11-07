33b15bd5255920e91f9492978a39315585fecbb1
Audrey Tang <audreyt@audreyt.org>
2019-11-07 13:42:16 +0800

    * Count down to DD4

--- js/countdown.js
+++ js/countdown.js
@@ -30,7 +30,7 @@ CountdownTimer.prototype={
 function CDT(){
 
  // Set countdown limit
- var tl = new Date('2019/08/14 00:00:00');
+ var tl = new Date('2019/11/22 00:00:00');
 
  // You can add time's up message here
  var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div><span class="number end"></span></span>');
